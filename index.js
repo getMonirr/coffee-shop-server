f4782b9854ef130c7df0829658647b83d0dd9303
Monir Hoshen <getmonirr@gmail.com>
2023-05-09 10:18:21 +0600

    crud project done

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get("/", (req, res) => {
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5ku26o.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -61,6 +60,44 @@ async function run() {
             const result = await coffeeCollection.deleteOne(query);
             res.send(result);
         });
+
+        // get a individual coffee
+        app.get('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+
+            res.send(result);
+        })
+
+        // update a individual coffee
+
+        app.put('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedCoffeeInfo = req.body;
+            console.log(updatedCoffeeInfo);
+
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateCoffee = {
+                $set: {
+                    name: updatedCoffeeInfo?.name,
+                    chef: updatedCoffeeInfo?.chef,
+                    supplier: updatedCoffeeInfo?.supplier,
+                    taste: updatedCoffeeInfo?.taste,
+                    category: updatedCoffeeInfo?.category,
+                    details: updatedCoffeeInfo?.details,
+                    photourl: updatedCoffeeInfo?.photo
+                }
+            }
+
+            const result = await coffeeCollection.updateOne(filter, updateCoffee, options);
+
+            res.send(result);
+        })
+
+
 
         // coffee shop server end
 
